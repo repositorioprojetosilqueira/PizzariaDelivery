88590787566b5a33500e97271362c665b6aaaf6f
ÍtaloSilqueira <italobrunosilqueira@gmail.com>
2017-10-01 19:16:50 -0300

    Tela nova

--- src/grafica/gPedidos.java
+++ src/grafica/gPedidos.java
@@ -5,12 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JInternalFrame;
 
 import View.vTelaPadrao;
+import javax.swing.JCheckBox;
 
 public class gPedidos extends vTelaPadrao {
 
 
 	public gPedidos() {
 			super("Pedidos", " ");
+			
+			JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
+			chckbxNewCheckBox.setBounds(133, 107, 97, 23);
+			jpCentro.add(chckbxNewCheckBox);
 	}
-
 }
