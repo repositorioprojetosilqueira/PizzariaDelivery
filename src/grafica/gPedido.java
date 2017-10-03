231713b3cdbd1755e98667b96e6eeee5bbfafaf7
jessicascruz <jessica.santos648@gmail.com>
2017-10-03 16:44:56 -0300

    TelaPedido

--- src/grafica/gPedido.java
+++ src/grafica/gPedido.java
@@ -18,6 +18,7 @@ public class gPedido extends vTelaPadrao {
 	private JTextField textField_5;
 	private JTextField textField_6;
 	private JTextField textField_7;
+	private JTextField textField_8;
 
 
 	public gPedido() {
@@ -26,16 +27,16 @@ public class gPedido extends vTelaPadrao {
 		jpCentro.setLayout(null);
 		
 		JLabel lblTelefone = new JLabel("Telefone:");
-		lblTelefone.setBounds(10, 11, 61, 14);
+		lblTelefone.setBounds(10, 8, 61, 14);
 		jpCentro.add(lblTelefone);
 		
 		textField = new JTextField();
-		textField.setBounds(81, 8, 86, 20);
+		textField.setBounds(10, 22, 86, 20);
 		jpCentro.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnBusca = new JButton("Busca");
-		btnBusca.setBounds(177, 7, 76, 23);
+		btnBusca.setBounds(106, 21, 76, 23);
 		jpCentro.add(btnBusca);
 		
 		JLabel lblCliente = new JLabel("Cliente:");
@@ -43,67 +44,76 @@ public class gPedido extends vTelaPadrao {
 		jpCentro.add(lblCliente);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(66, 39, 162, 20);
+		textField_1.setBounds(10, 56, 162, 20);
 		jpCentro.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(10, 84, 86, 20);
+		textField_2.setBounds(10, 104, 86, 20);
 		jpCentro.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(117, 84, 86, 20);
+		textField_3.setBounds(130, 104, 98, 20);
 		jpCentro.add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblTelefone_1 = new JLabel("Telefone");
-		lblTelefone_1.setBounds(10, 67, 61, 14);
+		lblTelefone_1.setBounds(10, 87, 61, 14);
 		jpCentro.add(lblTelefone_1);
 		
 		JLabel label = new JLabel("Telefone");
-		label.setBounds(120, 67, 61, 14);
+		label.setBounds(130, 87, 61, 14);
 		jpCentro.add(label);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(10, 125, 218, 20);
+		textField_4.setBounds(10, 145, 218, 20);
 		jpCentro.add(textField_4);
 		textField_4.setColumns(10);
 		
 		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
-		lblEndereo.setBounds(10, 110, 61, 14);
+		lblEndereo.setBounds(10, 130, 61, 14);
 		jpCentro.add(lblEndereo);
 		
 		textField_5 = new JTextField();
-		textField_5.setBounds(10, 166, 218, 20);
+		textField_5.setBounds(10, 186, 218, 20);
 		jpCentro.add(textField_5);
 		textField_5.setColumns(10);
 		
 		JLabel lblBairro = new JLabel("Bairro:");
-		lblBairro.setBounds(10, 149, 46, 14);
+		lblBairro.setBounds(10, 169, 46, 14);
 		jpCentro.add(lblBairro);
 		
 		JLabel lblReferncia = new JLabel("Refer\u00EAncia:");
-		lblReferncia.setBounds(10, 189, 76, 14);
+		lblReferncia.setBounds(10, 209, 76, 14);
 		jpCentro.add(lblReferncia);
 		
 		textField_6 = new JTextField();
-		textField_6.setBounds(10, 204, 218, 20);
+		textField_6.setBounds(10, 224, 218, 20);
 		jpCentro.add(textField_6);
 		textField_6.setColumns(10);
 		
 		textField_7 = new JTextField();
-		textField_7.setBounds(351, 8, 201, 20);
+		textField_7.setBounds(352, 22, 218, 20);
 		jpCentro.add(textField_7);
 		textField_7.setColumns(10);
 		
 		JLabel lblDescrio = new JLabel("Descri\u00E7\u00E3o:");
-		lblDescrio.setBounds(296, 11, 61, 14);
+		lblDescrio.setBounds(352, 8, 61, 14);
 		jpCentro.add(lblDescrio);
 		
 		JButton btnBusca_1 = new JButton("Busca");
-		btnBusca_1.setBounds(597, 7, 76, 23);
+		btnBusca_1.setBounds(580, 21, 76, 23);
 		jpCentro.add(btnBusca_1);
+		
+		JLabel lblQtde = new JLabel("Qtde");
+		lblQtde.setBounds(278, 8, 46, 14);
+		jpCentro.add(lblQtde);
+		
+		textField_8 = new JTextField();
+		textField_8.setBounds(278, 22, 54, 20);
+		jpCentro.add(textField_8);
+		textField_8.setColumns(10);
 
 	}
 }
