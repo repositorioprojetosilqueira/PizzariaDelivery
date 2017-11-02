1901d1ae276ee372307a75fb6fa8b4c0c427550b
jessicascruz <jessica.santos648@gmail.com>
2017-11-02 20:18:01 -0200

    Erro

--- src/grafica/login.java
+++ src/grafica/login.java
@@ -121,7 +121,7 @@ public class login extends JDialog implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 		if(e.getSource()==jbAcessar) {
 			new vPrincipal();
