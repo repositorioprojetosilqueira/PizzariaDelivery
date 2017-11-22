33fdeb3733850708dd52d0d463b4564d046a3cd3
jessicascruz <jessica.santos648@gmail.com>
2017-11-21 23:09:17 -0200

    fgh

--- src/controller/main.java
+++ src/controller/main.java
@@ -23,7 +23,7 @@ public class main {
             ex.printStackTrace();   Teste 
        } */
 		
-		//teste
+		
 		
 		try {   
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
