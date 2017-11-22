0a39f1b2e1e2adadca52ce5ab21c194140d1b72d
jessicascruz <jessica.santos648@gmail.com>
2017-11-22 15:42:10 -0200

    DaoPedido

--- src/model/dao/daoPedido.java
+++ src/model/dao/daoPedido.java
@@ -1,4 +1,4 @@
-/*package model.dao;
+package model.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,131 +6,127 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import model.mCliente;
 import model.mPedido;
 
 public class daoPedido extends DAOSuperClass<mPedido>{
-	
+
+
+
 	public daoPedido() throws Exception {
 		super();
-		
+
 	}
+
 
 	@Override
-	public boolean insert(daoPedido arg0) throws SQLException {
-		
+	public boolean insert(mPedido arg0) throws SQLException {
+
 		String sql = "INSERT INTO pedido (peFormaPagamento,peStatus,peTxEntrega,peVlTotal,peTotalProdutos) "
 				+ "VALUES(?,?,?,?,?);";
-	
+
 		PreparedStatement stm = this.createPreparedStatement(sql);
-		
-		stm.setString(1, arg0.getpeFormaPagamento());
-		stm.setString(2, arg0.getpeStatus());
-		stm.setString(3, arg0.getpeTxEntrega());
-		stm.setString(4, arg0.getpeVlTotal());
-		stm.setString(5, arg0.getpeTotalProdutos());
-		
+
+		stm.setString(1, arg0.getPeFormaPagamento());
+		//stm.setBoolean(2, arg0.getpeStatus());
+		stm.setString(3, arg0.getPeTxEntrega());
+		stm.setDouble(4, arg0.getPeVlTotal());
+		stm.setInt(5, arg0.getPeTotalProdutos());
+
 		boolean retorno = stm.executeUpdate() > 0;
-		
+
 		close(stm);
 		fecha();
-		
-		return retorno;
-	}
 
+		return retorno;
+
+	}
 
 	@Override
 	public boolean update(mPedido arg0) throws SQLException {
-		String sql = "UPDATE cliente SET  cNome = ?, cTelefone1 =?, cTelefone2 =?, cRua =?, cNumero =?, cBairro =?, cComplemento =?,"
-				+ "cRefEntrega =?, cHistCompras =? WHERE codCliente =?; ";
+
+		String sql = "UPDATE pedido SET  peFormaPagamento = ?, peStatus =?, peTxEntrega =?, peTotalProdutos =?,"
+				+ " WHERE codPedido =?; ";
 
 		PreparedStatement stm = this.createPreparedStatement(sql);
-		
-		stm.setString(1, arg0.getcNome());
-		stm.setString(2, arg0.getcTelefone1());
-		stm.setString(3, arg0.getcTelefone2());
-		stm.setString(4, arg0.getcRua());
-		stm.setString(5, arg0.getcNumero());
-		stm.setString(6, arg0.getcBairro());
-		stm.setString(7, arg0.getcComplemento());
-		stm.setString(8, arg0.getcRefEntrega());
-		stm.setString(9, arg0.getcHistCompras());
-		stm.setInt(10, arg0.getCodCliente());
-		
+
+		stm.setString(1, arg0.getPeFormaPagamento());
+		//stm.setBoolean(2, arg0.getpeStatus());
+		stm.setString(3, arg0.getPeTxEntrega());
+		stm.setDouble(4, arg0.getPeVlTotal());
+		stm.setInt(5, arg0.getPeTotalProdutos());
+
 		boolean retorno = stm.executeUpdate() > 0;
-		
+
 		close(stm);
 		fecha();
-		
+
 		return retorno;
 	}
 
 	@Override
 	public boolean delete(int codigo) throws SQLException {
-		String sql = "DELETE FROM cliente WHERE codCliente =?;";
-		
+
+		String sql = "DELETE FROM pedido WHERE codPedido =?;";
+
 		PreparedStatement stm = this.createPreparedStatement(sql);
 
 		stm.setInt(1, codigo);
-		
+
 		boolean retorno = stm.executeUpdate() > 0;
 		close(stm);
-		
+
 		return retorno;
 	}
 
 	@Override
-	public mCliente select(int codigo) throws SQLException {
-		String sql = "Select * from cliente where codCliente= ?";
+	public mPedido select(int codigo) throws SQLException {
+
+		String sql = "Select * from pedido where codPedido= ?";
+
 		PreparedStatement stm = this.createPreparedStatement(sql);
 		stm.setInt(1, codigo);
-		
+
 		ResultSet rs = stm.executeQuery();
-		mCliente retorno = null;
-		
+		mPedido retorno = null;
+
 		if(rs.next()) {
-			
-			retorno = new mCliente();
-			retorno.setCodCliente(rs.getInt(1));
-			retorno.setcNome(rs.getString(2));
-			retorno.setcTelefone1(rs.getString(3));
-			retorno.setcTelefone2(rs.getString(4));
-			retorno.setcRua(rs.getString(5));
-			retorno.setcNumero(rs.getString(6));
-			retorno.setcBairro(rs.getString(7));
-			retorno.setcComplemento(rs.getString(8));
-			retorno.setcRefEntrega(rs.getString(9));
-			retorno.setcHistCompras(rs.getString(10));
-			
+
+			retorno = new mPedido();
+			retorno.setCodPedido(rs.getInt(1));
+			retorno.setPeFormaPagamento(rs.getString(2));
+			retorno.setPeStatus(rs.getBoolean(3));
+			retorno.setPeTxEntrega(rs.getString(4));
+			retorno.setPeVlTotal(rs.getDouble(5));
+			retorno.setPeTotalProdutos(rs.getInt(6));
+			retorno.setPe_codCliente(rs.getInt(7));
+
 		}
-		
+
 		close(rs,stm);
-		
+
 		return retorno;
-		
 	}
 
 	@Override
-	public List<mCliente> selectAll() throws SQLException {
-		String sql = "SELECT * FROM cliente";
-		
+	public List<mPedido> selectAll() throws SQLException {
+
+		String sql = "SELECT * FROM pedido";
+
 		PreparedStatement stm = this.createPreparedStatement(sql);
-		
+
 		ResultSet rs = stm.executeQuery();
-		List<mCliente> retorno = new ArrayList();
+		List<mPedido> retorno = new ArrayList();
 		
 		while(rs.next()) {
-			mCliente temp = new mCliente();
-			temp.setCodCliente(rs.getInt(1));
-			temp.setcNome(rs.getString(2));
-			temp.setcTelefone1(rs.getString(3));
-			temp.setcTelefone2(rs.getString(4));
-			temp.setcRua(rs.getString(5));
-			temp.setcNumero(rs.getString(6));
-			temp.setcBairro(rs.getString(7));
-			temp.setcComplemento(rs.getString(8));
-			temp.setcRefEntrega(rs.getString(9));
-			temp.setcHistCompras(rs.getString(10));
+			mPedido temp = new mPedido();
+			temp.setCodPedido(rs.getInt(1));
+			temp.setPeFormaPagamento(rs.getString(2));
+			temp.setPeStatus(rs.getBoolean(3));
+			temp.setPeTxEntrega(rs.getString(4));
+			temp.setPeVlTotal(rs.getDouble(5));
+			temp.setPeTotalProdutos(rs.getInt(6));
+			temp.setPe_codCliente(rs.getInt(7));
 			
 			retorno.add(temp);
 			
@@ -138,8 +134,7 @@ public class daoPedido extends DAOSuperClass<mPedido>{
 		
 		close(rs,stm);
 		fecha();
-		return retorno;
-	}
+		return retorno;	}
 
 	@Override
 	public mPedido selectDesc(String descricao) throws SQLException {
@@ -147,17 +142,5 @@ public class daoPedido extends DAOSuperClass<mPedido>{
 		return null;
 	}
 
-	@Override
-	public boolean insert(mPedido arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update(mPedido arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
-*/
